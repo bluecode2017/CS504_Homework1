d448e46712c640813a46a30a5911a3b4522aad73
vagrant <vagrant>
2017-04-12 17:06:10 -0400

    Modify project to use mysql 1. modify pom.xml: 2. modify application.properties 3. add docker-compose.yml, install mysql

--- upload-running-informations.sh
+++ upload-running-informations.sh
@@ -1,1 +1,2 @@
-curl -H "Content-type: application/json" localhost:9005/bulk/runningInformationsUpload  -d @runningInformations.json
+#!/usr/bin/env bash
+curl -H "Content-type: application/json" localhost:8080/bulkUpload  -d @runningInformations.json
